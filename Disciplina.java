1f6b06fd9bca3a6cb4e47bb90c1ff7cb0e09beee
Gabrielly Magalhães <157931491+gabysmm@users.noreply.github.com>
2024-09-12 20:53:45 +0000

    classes de alunos, disciplina e professores

---
+++ Disciplina.java
@@ -0,0 +1,23 @@
+public class Disciplina {
+    private String nome_d;
+    private String codigo;
+
+    public Disciplina(String nome_d, String codigo) {
+        this.nome_d = nome_d;
+        this.codigo = codigo;
+    }
+
+    public String getNome_d() {
+        return nome_d;
+    }
+    public void setNome_d(String nome_d) {
+        this.nome_d = nome_d;
+    }
+    
+    public String getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+}
---
+++ Professor.java
@@ -0,0 +1,23 @@
+public class Professor {
+    private String nome_p;
+    private String identificacao;
+
+    public Professor(String nome_p, String identificacao) {
+        this.nome_p = nome_p;
+        this.identificacao = identificacao;
+    }
+
+    public String getNome_p(){
+        return this.nome_p;
+    }
+    public void nome_p(String nome_p){
+        this.nome_p = nome_p;
+    }
+
+    public String identificacao(){
+        return this.identificacao;
+    }
+    public void identificacao(String identificacao){
+        this.identificacao = identificacao;
+    }
+}
