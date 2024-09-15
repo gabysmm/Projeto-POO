ff47cb72808a0cee701167cc406bb97936424ac2
Laura <140566130+laurabluna@users.noreply.github.com>
2024-09-15 23:41:03 +0000

    tratamento de erros

--- Professores.java
+++ Professores.java
@@ -2,7 +2,10 @@ public class Professores {
     private String nome_p;
     private String identificacao;
 
-    public Professor(String nome_p, String identificacao) {
+    public professores(String nome_p, String identificacao) throws DadosInvalidosException {
+        if (nome_p == null || nome_p.isEmpty()) {
+            throw new DadosInvalidosException("O nome do professor é um campo obrigatório.");
+        }
         this.nome_p = nome_p;
         this.identificacao = identificacao;
     }
