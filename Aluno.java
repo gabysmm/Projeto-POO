e9af771bd5cf5a2fc209a25cc229c4f409e9d6b2
Laura <140566130+laurabluna@users.noreply.github.com>
2024-09-19 00:05:31 +0000

    mudando escopo

---
+++ Aluno.java
@@ -0,0 +1,27 @@
+public class Aluno {
+    private String nome;
+    private String matricula;
+
+    public Aluno(String nome, String matricula) throws DadosInvalidosException {
+        if (nome == null || nome.isEmpty()) {
+            throw new DadosInvalidosException("o nome do aluno é um campo obrigatório");
+        }
+        this.nome = nome;
+        this.matricula = matricula;
+        
+    }
+    public String getNome(){
+        return this.nome;
+    }
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getMatricula(){
+        return this.matricula;
+    }
+    public void setMatricula(String matricula){
+        this.matricula = matricula;
+    }
+
+}
